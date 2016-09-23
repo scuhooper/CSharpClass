8ad17524f57622c4392baca59f233d6ab8e187e9
scuhooper <keeling18@marshall.edu>
2016-09-23 18:53:32 -0400

    Finished program. Uses LINQ to find data at end.

--- ist303_keeling_j_a3/ist303_keeling_j_a3/Player.cs
+++ ist303_keeling_j_a3/ist303_keeling_j_a3/Player.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ist303_keeling_j_a3
 {
+    // enum class to track which choice was performed
 	enum Actions
 	{
 		eRock,
 		ePaper,
 		eScissors
 	}
+    // enum class to keep who won or if the game was a draw
 	enum Results
 	{
 		eDraw,
@@ -42,14 +45,14 @@ namespace ist303_keeling_j_a3
 		}
 
 		// constructor that only takes a parameter for the name of the player. Wins and Losses are always 0 when a player is created
-		Player( string _name )
+		public Player( string _name )
 		{
 			Name = _name;
 			Wins = 0;
 			Losses = 0;
 		}
 		// play a round of rock, paper, scissors
-		void PlayGame( Player p2 )
+		public void PlayGame( Player p2 )
 		{
 			Results result;	// variable to store the result of the round
 			// run at least one round
@@ -60,10 +63,13 @@ namespace ist303_keeling_j_a3
 				Actions p2Choice = (Actions)random.Next( 0, 3 );
 
 				result = DetermineWinner( p1Choice, p2Choice );	// check for a winner
+				PrintGameDetails( this, p2, p1Choice, p2Choice, result );
+				Thread.Sleep( 1000 );	// wait one second before continuing
+				
 			}
 			while ( result == Results.eDraw );	// keep going until someone wins
 
-			if ( result == Results.ePlayer1Wins )
+			if ( result == Results.ePlayer1Wins )   // update the wins and losses for both players according to who won
 			{
 				this.Wins++;
 				p2.Losses++;
@@ -74,10 +80,11 @@ namespace ist303_keeling_j_a3
 				p2.Wins++;
 			}
 		}
-
+        
+        // figure out winner based on the actions chosen. return draw or who won
 		Results DetermineWinner( Actions action1, Actions action2 )
 		{
-			if ( action1 == Actions.eRock )
+			if ( action1 == Actions.eRock ) // player1 is rock
 			{
 				if ( action2 == Actions.eScissors )
 					return Results.ePlayer1Wins;
@@ -86,7 +93,7 @@ namespace ist303_keeling_j_a3
 				else
 					return Results.eDraw;
 			}
-			else if ( action1 == Actions.ePaper )
+			else if ( action1 == Actions.ePaper )   // player1 is paper
 			{
 				if ( action2 == Actions.eRock )
 					return Results.ePlayer1Wins;
@@ -95,7 +102,7 @@ namespace ist303_keeling_j_a3
 				else
 					return Results.eDraw;
 			}
-			else if ( action1 == Actions.eScissors )
+			else if ( action1 == Actions.eScissors )    // player1 is scissors
 			{
 				if ( action2 == Actions.ePaper )
 					return Results.ePlayer1Wins;
@@ -105,7 +112,49 @@ namespace ist303_keeling_j_a3
 					return Results.eDraw;
 			}
 			else
-				return Results.eDraw;
+				return Results.eDraw;   // nothing should reach this point, but if it does, redo the round
+		}
+
+		// output the details of each round to the screen. Tell who did what, and who, if anyone, won
+		void PrintGameDetails( Player p1, Player p2, Actions action1, Actions action2, Results result ) {
+			Console.WriteLine( "{0} chose {1}! {2} chose {3}!", p1.Name, ActionToString( action1 ), p2.Name, ActionToString( action2 ) );
+			Console.WriteLine( ResultToString( p1, p2, result ) );
+			Console.WriteLine();
+		}
+
+		// simply converts the action enum into its string equivalent
+		string ActionToString( Actions action ) {
+			string actionString;
+			if ( action == Actions.ePaper ) {
+				actionString = "paper";
+				return actionString;
+			}
+			else if ( action == Actions.eRock ) {
+				actionString = "rock";
+				return actionString;
+			}
+			else {
+				actionString = "scissors";
+				return actionString;
+			}
+		}
+
+		// converts the Results enum to the string equivalent
+		string ResultToString( Player p1, Player p2, Results result ) {
+			string strResult;
+
+			if ( result == Results.eDraw ) {
+				strResult = "DRAW!";
+				return strResult;
+			}
+			else if ( result == Results.ePlayer1Wins ) {
+				strResult = p1.Name + " wins!";
+				return strResult;
+			}
+			else {
+				strResult = p2.Name + " wins!";
+				return strResult;
+			}
 		}
 	}
 }
--- ist303_keeling_j_a3/ist303_keeling_j_a3/Program.cs
+++ ist303_keeling_j_a3/ist303_keeling_j_a3/Program.cs
@@ -8,9 +8,75 @@ namespace ist303_keeling_j_a3
 {
 	class Program
 	{
-		static void Main( string[] args )
-		{
+		static void Main( string[] args ) {
+			// not needed
+			//bool bIsAddingPlayer = true;
+			//List<string> nameList = new List<string>();
+			//while ( bIsAddingPlayer ) {
+			//	Console.WriteLine( "Enter the name of the player to be added to the game:" );
+			//	string tempName = Console.ReadLine();
+			//	nameList.Add( tempName );
 
+			//	Console.WriteLine( "There are {0} players right now. A minimum of 2 is needed to play.", players.Count() );
+			//	Console.WriteLine( "Would you like to add another player? Enter y/n" );
+			//	char choice;
+			//	char.TryParse( Console.ReadLine(), out choice );
+			//	char.ToLower( choice );
+
+			//	if ( choice == 'y' )
+			//		bIsAddingPlayer = true;
+			//	else
+			//		bIsAddingPlayer = false;
+			//}
+
+			// names of players. can input more as long as they are then added to the GetPlayerNames function
+			string name1 = "Geralt";
+			string name2 = "Edmond Dantes";
+			string name3 = "James Bond";
+			string name4 = "Lara Croft";
+			string name5 = "Peter Griffin";
+
+			// create list based on name# variables
+			List<Player> players = GetPlayerNames( name1, name2, name3, name4, name5 );
+			List<Player> alreadyPlayed = new List<Player>();	// empty list to hold players after they have finished their hands
+
+			// not sure if this is faster than using nested for loops (for example: int i=0, and int j=1)
+			// while loop to cycle through one player playing all remaining players before being sent to alreadyPlayed
+			while ( players.Count > 1 ) {	// while we still have 2 players waiting to play
+				for ( int i = 1; i < players.Count; i++ ) {
+					players[0].PlayGame( players[i] );	// play a round until there is a winner
+				}
+				alreadyPlayed.Add( players[0] );	// add player to already played list
+				players.Remove( players[0] );	// remove player from the active players list
+			}
+			// add and remove one last time to get the player who was at the end of the list originally
+			alreadyPlayed.Add( players[0] );
+			players.Remove( players[0] );
+
+			// LINQ to generate an array where players are sorted based on the total number of wins
+			var listSortedByWins = from player in alreadyPlayed orderby player.Wins descending select player;
+			// step through array and print the results
+			foreach ( Player player in listSortedByWins ) {
+				Console.WriteLine( "{0} has {1} wins and {2} losses.", player.Name, player.Wins, player.Losses );
+			}
+
+			Console.WriteLine();	// needed for spacing and ease of reading the output
+
+			// LINQ to generate an array where players have same number of wins and losses
+			var EqualWinLoss = from player in alreadyPlayed where player.Wins == player.Losses select player;
+			// step through array and print the results
+			foreach ( Player player in EqualWinLoss ) {
+				Console.WriteLine( "{0} has an equal amount wins and losses.", player.Name );
+			}
+		}
+
+		// function to take in unlimited names as strings and create a singular list of players from them. Returns the list of players.
+		static List<Player> GetPlayerNames( params string[] list ){
+			List<Player> tempList = new List<Player>();
+			foreach ( string name in list ) {
+				tempList.Add( new Player( name ) );
+			}
+			return tempList;
 		}
 	}
 }
