143f801d303a52f97a539640bd3dd166c9f31fea
scuhooper <keeling18@marshall.edu>
2016-12-08 20:50:29 -0500

    ToDoList Initial Commit

---
+++ ist303_keeling_j_a6/ist303_keeling_j_a6/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ist303_keeling_j_a6
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && ( components != null ) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(821, 696);
+			this.Name = "frmMain";
+			this.Text = "To-Do-List";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
---
+++ ist303_keeling_j_a6/ist303_keeling_j_a6/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ist303_keeling_j_a6
+{
+	public partial class frmMain: Form
+	{
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+	}
+}
