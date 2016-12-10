58fcaa27a046dd8af87e71a22209be4c6fa0c5b4
scuhooper <keeling18@marshall.edu>
2016-12-10 00:47:28 -0500

    Directory Displayer initial commit
    Directory Displayer will show contents of all subfolders and all files.
    Need to implement async/await.
    Need to make use of checkbox for if subdirectories are needed.

---
+++ ist303_keeling_j_a7/ist303_keeling_j_a7/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ist303_keeling_j_a7
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && ( components != null ) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtFileList = new System.Windows.Forms.RichTextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.chkSubdirectories = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// txtFileList
+			// 
+			this.txtFileList.Location = new System.Drawing.Point(13, 14);
+			this.txtFileList.Name = "txtFileList";
+			this.txtFileList.Size = new System.Drawing.Size(606, 429);
+			this.txtFileList.TabIndex = 0;
+			this.txtFileList.Text = "";
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Location = new System.Drawing.Point(13, 449);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(94, 45);
+			this.btnBrowse.TabIndex = 1;
+			this.btnBrowse.Text = "&Browse";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// chkSubdirectories
+			// 
+			this.chkSubdirectories.AutoSize = true;
+			this.chkSubdirectories.Location = new System.Drawing.Point(113, 464);
+			this.chkSubdirectories.Name = "chkSubdirectories";
+			this.chkSubdirectories.Size = new System.Drawing.Size(195, 17);
+			this.chkSubdirectories.TabIndex = 2;
+			this.chkSubdirectories.Text = "Include &Subdirectories and Folders?";
+			this.chkSubdirectories.UseVisualStyleBackColor = true;
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(631, 562);
+			this.Controls.Add(this.chkSubdirectories);
+			this.Controls.Add(this.btnBrowse);
+			this.Controls.Add(this.txtFileList);
+			this.Name = "frmMain";
+			this.Text = "Directory Displayer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox txtFileList;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.CheckBox chkSubdirectories;
+	}
+}
+
---
+++ ist303_keeling_j_a7/ist303_keeling_j_a7/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ist303_keeling_j_a7
+{
+	public partial class frmMain: Form
+	{
+		string path;
+
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void btnBrowse_Click( object sender, EventArgs e )
+		{
+			FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+			folderBrowser.ShowDialog();
+			path = folderBrowser.SelectedPath;
+			GetFolders( path );
+		}
+
+		void GetFolders( string newPath )
+		{
+			foreach ( string str in Directory.GetDirectories( newPath ) )
+			{
+				txtFileList.AppendText( str + "\n" );
+				GetFiles( str );
+				GetFolders( str );
+			}
+		}
+		void GetFiles( string newPath )
+		{
+			foreach ( string str in Directory.GetFiles( newPath ) )
+			{
+				txtFileList.AppendText( str + "\n" );
+			}
+		}
+	}
+}
